5d89633c51ac089402ecc47601381b5b12c418c9
Alistair King <alistair@caida.org>
2015-11-20 07:28:58 -0800

    Generate probelists in JSON format

--- tools/gen-probelist.c
+++ tools/gen-probelist.c
@@ -17,7 +17,11 @@
  *
  */
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -43,7 +47,6 @@
 
 #define NETACQ_METRIC_PREFIX "geo.netacuity"
 #define PFX2AS_METRIC_PREFIX "asn"
-#define BLOCKS_METRIC_PREFIX "blocks"
 
 KHASH_SET_INIT_STR(strset);
 static khash_t(strset) *keyset = NULL;
@@ -57,6 +60,9 @@ static ipmeta_provider_t *pfx2as_provider;
 static char *netacq_loc_file = NULL;
 static char *netacq_blocks_file = NULL;
 static char *pfx2as_file = NULL;
+
+// probelist date
+static char *date = NULL;
 
 /* should only summary stats be dumped? */
 static int summary_only = 0;
@@ -73,16 +79,24 @@ static int slash24_cnt = 0;
 static int resp_slash24_cnt = 0;
 static int usable_slash24_cnt = 0;
 
+int first_object = 1; // don't write the comma for the previous obj
+
 static void usage(char *name)
 {
   fprintf(stderr,
-          "Usage: %s [-s] -f history-file -l netacq-locations-file -b netacq-blocks-file -p pfx2as-file\n"
-          "       -s          only dump summary stats\n",
+          "Usage: %s [-s] -bdflp\n"
+          "       -d <YYYY-MM-DD>  date of the probelist (required)\n"
+          "       -f <file>        history file (required)\n"
+          "       -l <file>        net acuity locations file (required)\n"
+          "       -b <file>        net acuity blocks file (required)\n"
+          "       -p <file>        prefix2as file (required)\n"
+          "       -s               only dump summary stats\n",
           name);
 }
 
 static void cleanup()
 {
+  free(date);
   free(history_file);
   free(netacq_loc_file);
   free(netacq_blocks_file);
@@ -97,6 +111,9 @@ static void cleanup()
 static void dump_slash24_info()
 {
   khiter_t k;
+  int comma = 0;
+  char ip_str[INET_ADDRSTRLEN];
+  uint32_t tmp;
 
   slash24_cnt++;
 
@@ -119,26 +136,77 @@ static void dump_slash24_info()
     return;
   }
 
-  // print the /24 stats:
-  // ## <slash24> <resp_ip_cnt> <avg resp rate>
-  fprintf(stdout, "SLASH24 %x %d %f\n", last_slash24, e_b_cnt, avg);
-  for (k=kh_begin(keyset); k < kh_end(keyset); k++) {
-    if (kh_exist(keyset, k)) {
-      fprintf(stdout, "SLASH24_META %s\n", kh_key(keyset, k));
-    }
+  // add a comma for the previous JSON object if this is not the first
+  if (first_object == 0) {
+    fprintf(stdout, ",\n");
+  } else {
+    first_object = 0;
   }
 
+  // convert the /24 to a string
+  tmp = htonl(last_slash24);
+  inet_ntop(AF_INET, &tmp, ip_str, INET_ADDRSTRLEN);
+
+  // start the JSON object
+  fprintf(stdout, "  \"%s/24\": {\n", ip_str);
+
+  // print the /24 stats:
+  // ## <slash24> <resp_ip_cnt> <avg resp rate>
+  fprintf(stdout,
+          "    \"date\": \"%s\",\n"
+          "    \"host_cnt\": %d,\n"
+          "    \"avg_resp_rate\": %f,\n",
+          date,
+          e_b_cnt,
+          avg);
+
+  fprintf(stdout, "    \"meta\": [\n");
+  for (k=kh_begin(keyset); k < kh_end(keyset); k++) {
+    if (!kh_exist(keyset, k)) {
+      continue;
+    }
+    if (comma != 0) {
+      fprintf(stdout, ",\n");
+    } else {
+      comma = 1;
+    }
+    fprintf(stdout, "      \"%s\"", kh_key(keyset, k));
+  }
+  fprintf(stdout, "\n"); // end the last meta line
+  fprintf(stdout, "    ],\n");
+
+  fprintf(stdout, "    \"hosts\": [\n");
   // print each ip address
+  comma = 0;
   int last_octet;
   for (last_octet = 0; last_octet < 256; last_octet++) {
     if (e_b[last_octet] == UNSET) {
       // this ip was never responsive
       continue;
     }
-    fprintf(stdout, "%x %f\n",
-            last_slash24 | last_octet,
+
+    // convert the host to a string
+    tmp = htonl((last_slash24 | last_octet));
+    inet_ntop(AF_INET, &tmp, ip_str, INET_ADDRSTRLEN);
+
+    if (comma != 0) {
+      fprintf(stdout, ",\n");
+    } else {
+      comma = 1;
+    }
+    fprintf(stdout, "      {\n");
+    fprintf(stdout,
+            "        host_ip: \"%s\",\n"
+            "        e_b: %f\n",
+            ip_str,
             e_b[last_octet] / 4.0);
+    fprintf(stdout, "      }");
   }
+  fprintf(stdout, "\n"); // end the last host line
+  fprintf(stdout, "    ]\n");
+
+  // end the JSON object
+  fprintf(stdout, "  }");
 }
 
 static int lookup_metadata()
@@ -295,7 +363,7 @@ int main(int argc, char **argv)
   memset(e_b, UNSET, sizeof(uint8_t) * 256);
 
   while (prevoptind = optind,
-         (opt = getopt(argc, argv, ":b:f:l:p:s?")) >= 0) {
+         (opt = getopt(argc, argv, ":b:d:f:l:p:s?")) >= 0) {
     if (optind == prevoptind + 2 &&
         optarg && *optarg == '-' && *(optarg+1) != '\0') {
       opt = ':';
@@ -305,6 +373,11 @@ int main(int argc, char **argv)
     case 'b':
       netacq_blocks_file = strdup(optarg);
       assert(netacq_blocks_file != NULL);
+      break;
+
+    case 'd':
+      date = strdup(optarg);
+      assert(date != NULL);
       break;
 
     case 'f':
@@ -352,6 +425,13 @@ int main(int argc, char **argv)
 
   if (history_file == NULL) {
     fprintf(stderr, "ERROR: History file must be specified using -f\n");
+    usage(argv[0]);
+    cleanup();
+    return -1;
+  }
+
+  if (date == NULL) {
+    fprintf(stderr, "ERROR: Date must be specified using -d\n");
     usage(argv[0]);
     cleanup();
     return -1;
@@ -420,6 +500,10 @@ int main(int argc, char **argv)
     return -1;
   }
 
+  // start the JSON object
+  fprintf(stdout, "{\n");
+
+  // process the history file
   while (wandio_fgets(infile, buffer, 1024, 1) != 0) {
     if (buffer[0] == '#') {
       continue;
@@ -429,6 +513,9 @@ int main(int argc, char **argv)
       return -1;
     }
   }
+
+  // end the JSON object
+  fprintf(stdout, "\n}\n");
 
   // dump the final /24
   dump_slash24_info();
